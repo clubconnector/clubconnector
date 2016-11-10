7501a41df20658f31a749b0f244283c629bf6b13
Yohan Yang <yohany@hawaii.edu>
2016-11-10 09:47:45 -1000

    fixed

--- app/imports/ui/pages/index.js
+++ app/imports/ui/pages/index.js
@@ -10,11 +10,7 @@ import './browse-clubs-page.js';
 import './home-page.html';
 import './home-page.js';
 
-<<<<<<< HEAD
-import './user-home_page.html';
-import './user-home_page.js';
-=======
-
 import './cas-login.html';
 import './cas-login.js';
->>>>>>> master
+
+
