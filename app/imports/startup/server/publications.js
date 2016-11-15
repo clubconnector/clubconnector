47da2e1c4964b6c5f1adddb712aa8adfb74df0f6
rfiggs <rfiggs@hawaii.edu>
2016-11-15 08:13:35 -1000

    added fliters dropdown to browse page

--- app/imports/startup/server/publications.js
+++ app/imports/startup/server/publications.js
@@ -1,11 +1,18 @@
 import { Clubs } from '../../api/clubs/clubs.js';
 import { Users } from '../../api/users/users.js';
+import { Tags } from '../../api/tags/tags.js';
 import { Meteor } from 'meteor/meteor';
 
 Meteor.publish('Clubs', function publishClubsData() {
   return Clubs.find();
 });
 
+Meteor.publish('Tags', function publishClubsData() {
+  return Tags.find();
+});
+
 Meteor.publish('Users', function publishClubsData() {
   return Users.find();
 });
+
+
