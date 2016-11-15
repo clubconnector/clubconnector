92818a47e08c4f302e8fd35e7b6c88401f0dcfd9
rfiggs <rfiggs@hawaii.edu>
2016-11-14 17:16:37 -1000

    added tags database and seeds

--- app/client/main.js
+++ app/client/main.js
@@ -2,6 +2,7 @@ import '/imports/startup/client';
 import '/imports/startup/both';
 import '/imports/api/clubs';
 import '/imports/api/users';
+import '/imports/api/tags';
 import '/imports/ui/layouts';
 import '/imports/ui/pages';
 import '/imports/ui/stylesheets/style.css';
--- app/server/main.js
+++ app/server/main.js
@@ -2,3 +2,4 @@ import '/imports/startup/server';
 import '/imports/startup/both';
 import '/imports/api/clubs';
 import '/imports/api/users';
+import '/imports/api/tags';
