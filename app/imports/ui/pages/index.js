89c17bdd925805d851cc988ca6783c74c2e8c796
Wyatt Bartlett <wyattmmbartlett@gmail.com>
2016-11-15 07:00:39 -1000

    Footer changes + Fixes to settings.development.json

--- app/imports/ui/pages/index.js
+++ app/imports/ui/pages/index.js
@@ -19,9 +19,6 @@ import './cas-login.js';
 import './profile-page.html';
 import './profile-page.js';
 
-import './landing-page.html';
-import './landing-page.js';
-
 import './club-page.html';
 import './club-page.js';
 
