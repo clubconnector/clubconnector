a3b651bf0a37fa0a0b5837f36bf9d2355a67fc10
Keanu Robles <keanur@hawaii.edu>
2016-11-18 14:34:04 -1000

    Resolved dropdown issue
    There was an extra dropdown implementation in header.js that was
    conflicting with the one in las-login.js

--- app/imports/ui/layouts/header.js
+++ app/imports/ui/layouts/header.js
@@ -3,9 +3,9 @@ import { Template } from 'meteor/templating';
 
 // The Header menu does not use dropdown menus, but most menus do.
 // Here's how to do the required initialization for Semantic UI dropdown menus.
-Template.Header.onRendered(function enableDropDown() {
-  this.$('.dropdown').dropdown();
-});
+// Template.Header.onRendered(function enableDropDown() {
+//   this.$('.dropdown').dropdown();
+// });
 
 Template.Header.helpers({
   /**
