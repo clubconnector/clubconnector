f559f06261ffb49a1604c96dc2618ca1010742b3
Wyatt Bartlett <wyattmmbartlett@gmail.com>
2016-12-05 02:00:35 -1000

    Cleaned Up index.js

--- app/imports/ui/pages/index.js
+++ app/imports/ui/pages/index.js
@@ -7,17 +7,8 @@ import './edit-club-page.js';
 import './browse-clubs-page.html';
 import './browse-clubs-page.js';
 
-import './home-page.html';
-import './home-page.js';
-
-import './user-home-page.html';
-import './user-home-page.js';
-
 import './cas-login.html';
 import './cas-login.js';
-
-import './profile-page.html';
-import './profile-page.js';
 
 import './club-page.html';
 import './club-page.js';
@@ -27,10 +18,6 @@ import './club-about-page.js';
 
 import './club-members-page.html';
 import './club-members-page.js';
-
-/* import './calendar.html';
-import './calendar.js';
-*/
 
 import './filter-dropdown.html';
 import './filter-dropdown.js';
