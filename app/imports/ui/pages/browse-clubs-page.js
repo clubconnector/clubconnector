42ca153a4ac281240dd5e5be898f9ffc254e3fce
Yohan Yang <yohany@hawaii.edu>
2016-12-13 13:13:50 -1000

    temp2

--- app/imports/ui/pages/browse-clubs-page.js
+++ app/imports/ui/pages/browse-clubs-page.js
@@ -94,7 +94,7 @@ Template.Browse_Clubs_Page.onRendered(function go() {
 
 
 Template.registerHelper('currentAdmin', function () {
-  return Meteor.user() === 'siteAdmin';
+  return Meteor.user().siteAdmin;
 });
 
 
