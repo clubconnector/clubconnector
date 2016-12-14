599633622a23ebce53ed8d507786a088dd99c7df
rfiggs <rfiggs@hawaii.edu>
2016-12-13 19:55:55 -1000

    hooked up edit club page

--- app/imports/ui/pages/edit-club-page.js
+++ app/imports/ui/pages/edit-club-page.js
@@ -4,14 +4,22 @@ import { Template } from 'meteor/templating';
 import { Tracker } from 'meteor/tracker';
 import { _ } from 'meteor/underscore';
 import { Clubs, ClubsSchema } from '../../api/clubs/clubs.js';
+import { Tags } from '../../api/tags/tags.js';
 
 /* eslint-disable object-shorthand, no-unused-vars */
 
 const displayErrorMessages = 'displayErrorMessages';
+function tagsList() {
+  return Tags.find();
+}
+function club() {
+  return Clubs.findOne({ _id: FlowRouter.getParam('_id') });
+}
 
 Template.Edit_Club_Page.onCreated(function onCreated() {
   this.autorun(() => {
     this.subscribe('Clubs');
+    this.subscribe('Tags');
   });
   this.messageFlags = new ReactiveDict();
   this.messageFlags.set(displayErrorMessages, false);
@@ -31,19 +39,36 @@ Template.Edit_Club_Page.helpers({
     const errorKeys = Template.instance().context.invalidKeys();
     return _.find(errorKeys, (keyObj) => keyObj.name === fieldName);
   },
+  club,
+  tagsList,
 });
 
 Template.Edit_Club_Page.onRendered(function enableSemantic() {
   const template = this;
-  template.subscribe('StudentData', () => {
+
+  template.subscribe('Clubs', () => {
+     // Use this template.subscribe callback to guarantee that the following code executes after subscriptions OK.
+    Tracker.afterFlush(() => {
+      const dd = template.$('#typeDropdown.ui.selection.dropdown');
+      dd.dropdown({});
+      dd.dropdown('set selected', club().type);
+      const tagsDD = template.$('#tagsDropdown.ui.selection.dropdown');
+      tagsDD.dropdown({});
+      tagsDD.dropdown('set selected', club().tags);
+
+
+      // Use Tracker.afterFlush to guarantee that the DOM is re-rendered before calling JQuery.
+    });
+  });
+  template.subscribe('Tags', () => {
     // Use this template.subscribe callback to guarantee that the following code executes after subscriptions OK.
     Tracker.afterFlush(() => {
+      const tagsDD = template.$('#tagsDropdown.ui.selection.dropdown');
+      tagsDD.dropdown({});
+      tagsDD.dropdown('set selected', club().tags);
+
+
       // Use Tracker.afterFlush to guarantee that the DOM is re-rendered before calling JQuery.
-      template.$('select.ui.dropdown').dropdown();
-      template.$('.ui.selection.dropdown').dropdown();
-      template.$('select.dropdown').dropdown();
-      template.$('.ui.checkbox').checkbox();
-      template.$('.ui.radio.checkbox').checkbox();
     });
   });
 });
@@ -60,21 +85,28 @@ Template.Edit_Club_Page.events({
     const orgEmail = event.target.orgEmail.value;
     const orgWebsite = event.target.orgWebsite.value;
     const orgSocial = event.target.orgSocial.value;
+    const tags = event.target.tags.value.split(',');
+    const imgUrl = event.target.imgUrl.value;
+    const bannerimgUrl = event.target.bannerimgUrl.value;
+    const description = event.target.description.value;
 
-    const updatedClub = { type, orgName, acronym, contactName, contactEmail, orgEmail, orgWebsite, orgSocial };
+    const updatedClub = { type, orgName, acronym, contactName, contactEmail, orgEmail, orgWebsite, orgSocial,
+      tags, imgUrl, bannerimgUrl, description };
     // Clear out any old validation errors.
     instance.context.resetValidation();
     // Invoke clean so that newStudentData reflects what will be inserted.
     ClubsSchema.clean(updatedClub);
     // Determine validity.
+    console.log(updatedClub);
     instance.context.validate(updatedClub);
     if (instance.context.isValid()) {
       Clubs.update(FlowRouter.getParam('_id'), { $set: updatedClub });
       instance.messageFlags.set(displayErrorMessages, false);
-      FlowRouter.go('Home_Page');
+      FlowRouter.go('Browse_Clubs_Page');
     } else {
       instance.messageFlags.set(displayErrorMessages, true);
     }
+    console.log(Template.instance().context.invalidKeys());
   },
 
   'click .delete'(event, instance) {
@@ -85,7 +117,7 @@ Template.Edit_Club_Page.events({
       if (instance.context.isValid()) {
         Clubs.remove(FlowRouter.getParam('_id'));
         instance.messageFlags.set(displayErrorMessages, false);
-        FlowRouter.go('Home_Page');
+        FlowRouter.go('Browse_Clubs_Page');
       }
     }
   },
